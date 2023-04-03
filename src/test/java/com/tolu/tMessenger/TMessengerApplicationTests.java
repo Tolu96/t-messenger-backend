f3d644df3bc28592b668cdf71eac40e82dbb41f2
Tolunay Oezcan <tolunay.oezcan@hs-furtwangen.de>
2023-04-03 18:06:44 +0200

    Spring Project initialized

---
+++ src/test/java/com/tolu/tMessenger/TMessengerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tolu.tMessenger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TMessengerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
